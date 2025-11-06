b47746a654b722a159a45a7b0f07797cf2709f24
Sebastian <storo3@outlook.com>
2025-11-06 17:06:53 -0500

    fix migration dtype

--- migrations/20251022195517_ctf_join.sql
+++ migrations/20251022195517_ctf_join.sql
@@ -5,7 +5,7 @@ CREATE TABLE active_ctf_members (
     -- Channel ID of CTF
     channel_id INT NOT NULL,
     -- Discord ID of member who joined
-    member_id TEXT NOT NULL,
+    member_id INT NOT NULL,
     join_time TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(channel_id, member_id)
 );
